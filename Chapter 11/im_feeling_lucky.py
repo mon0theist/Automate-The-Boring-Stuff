be4acb58bca243cc91d73040d90e28709df436f4
mon0theist <bburns91@gmail.com>
2019-12-28 22:52:54 -0600

    still can't figure out why this won't work, but I suspect that the syntax for BeautifulSoup may have changed since ATBS was published. I can't get BeautifulSoup to pick up classes or ids.

--- Chapter 11/im_feeling_lucky.py
+++ Chapter 11/im_feeling_lucky.py
@@ -3,6 +3,10 @@
 # ATBS Chapter 11 - I'm Feeling Lucky
 #
 # Tutorial is out of date, doesn't work anymore, not sure how to fix it
+# I can get BeautifulSoup to find regular tags like <a> or <p> but it won't pick
+# up anything with a class or id. Can't figure out why.
+#
+# Test URL: https://www.google.com/search?q=Beautiful%20Soup
 
 import requests, sys, bs4, webbrowser
 
