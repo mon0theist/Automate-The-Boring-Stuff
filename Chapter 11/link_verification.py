428df1eab4db3d7c8de384bf61cd4a1e42b766d3
mon0theist <bburns91@gmail.com>
2020-01-01 22:48:50 -0600

    working on link_verification.py

--- Chapter 11/link_verification.py
+++ Chapter 11/link_verification.py
@@ -5,3 +5,12 @@
 # Write a program that, given the URL of a web page, will attempt to download
 # every linked page on the page. The program should flag any pages that have a
 # 404 “Not Found” status code and print them out as broken links.
+
+import requests, bs4
+
+res = requests.get(input('Please enter a URL: '))
+
+try:
+    res.raise_for_status()
+except Exception as exc:
+    print('Error: %s' % (exc))
