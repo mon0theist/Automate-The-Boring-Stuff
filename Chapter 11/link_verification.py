3738bc39091818cba7477d719c23e5e7ec200183
mon0theist <bburns91@gmail.com>
2020-01-01 21:32:26 -0600

    started link_verification.py

---
+++ Chapter 11/link_verification.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python
+#
+# ATBS Chapter 11 - Practice Project - Link Verification
+#
+# Write a program that, given the URL of a web page, will attempt to download
+# every linked page on the page. The program should flag any pages that have a
+# 404 “Not Found” status code and print them out as broken links.
