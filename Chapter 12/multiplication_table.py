45691dcea8cacbe996d976d78f71b083f29c189a
mon0theist <bburns91@gmail.com>
2020-01-10 06:46:47 -0600

    making progress on multiplication_table.py

--- Chapter 12/multiplication_table.py
+++ Chapter 12/multiplication_table.py
@@ -13,3 +13,37 @@
 # Row 1 and column A should be used for labels and should be in bold.
 
 import openpyxl, sys
+from openpyxl.utils import get_column_letter, column_index_from_string
+# DEPRECATED:
+# from openpyxl.cell import get_column_letter, column_index_from_string
+
+# Pseudocode
+# Create workbook, set sheet
+# N = sys.argv[1]
+# Check if N = int, otherwise error
+# rows = N
+# column_letter = get_column_letter(N)
+# minimum cell = ['A1']
+# maximum cell = [(get_column_letter(N) + str(N)]
+#
+# Figure out loop
+# for rows in range(1, N):
+#   for columns in range(1, N):
+#
+#
+# row = i
+# column = j
+
+
+# Create workbook, set sheet
+wb = openpyxl.Workbook()
+sheet = wb.active
+
+# Determine N
+N = sys.argv[1]
+if not type(N) == int:
+    print('Invalid argument: Please enter an int value\n')
+    exit()
+else:
+    # write cells
+    
