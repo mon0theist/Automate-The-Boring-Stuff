ef72c7ab8601dd9db5cfbad7f71b9ec1e053c83d
mon0theist <bburns91@gmail.com>
2020-01-16 16:44:17 -0600

    Completed multiplication_table.py, included some formatting for readability of results file

--- Chapter 12/multiplication_table.py
+++ Chapter 12/multiplication_table.py
@@ -9,69 +9,62 @@
 # py multiplicationTable.py 6
 #
 # ... it should create a spreadsheet that looks like Figure 12-11.
-#
-# Row 1 and column A should be used for labels and should be in bold.
-
 
 import openpyxl, sys
+from openpyxl.styles import Font
+from openpyxl.styles import Alignment
 from openpyxl.utils import get_column_letter, column_index_from_string
 # DEPRECATED:
 # from openpyxl.cell import get_column_letter, column_index_from_string
 
-# Pseudocode
-# Create workbook, set sheet
-# N = sys.argv[1]
-# Check if N = int, otherwise error
-# rows = N
-# column_letter = get_column_letter(N)
-# minimum cell = ['A1']
-# maximum cell = [(get_column_letter(N) + str(N)]
-#
-# Figure out loop
-# for rows in range(1, N):
-#   for columns in range(1, N):
-#
-#
-# row = i
-# column = j
-#
-# sheet.cell(row=i, column=j).value
-#
-# Excel Formula:
-# =[cell]*[cell]
-#
-# Outer Loop (for each column):
-#   Inner Loop (for each row in each column)
+# Determine n, validate user input
+try:
+    n = int(sys.argv[1])
 
+    # Create workbook, set sheet
+    wb = openpyxl.Workbook()
+    sheet = wb.active
 
-# Create workbook, set sheet
-wb = openpyxl.Workbook()
-sheet = wb.active
+    # Cell A1 should be blank, but this line is probably unnecessary
+    sheet.cell(row=1, column=1).value = ''
 
-# Determine N
-n = sys.argv[1]
-if not type(n) == int:
-    print('Invalid argument: Please enter an int value\n')
-    exit()
-else:
-    # Cell A1 should be blank
-    sheet.cell(row=1, column=1).value = 'BLANK'
+    # bold font object
+    bold_font = Font(bold=True)
+
+    print("Generating multiplication table...")
 
     # create x-axis (row 1, columns 2 thru n)
     for i in range(2, n+2):
         sheet.cell(row=1, column=i).value = str(i - 1)
-        #TESTING print(sheet.cell(row=1, column=i).value)
-        # figure out how to set text as Bold and Centered
+        # formatting
+        current_cell = sheet.cell(row=1, column=i)
+        current_cell.alignment = Alignment(horizontal='center')
+        current_cell.font = bold_font
 
     # create y-axis (column 1/A, rows 2 thru n)
     for i in range(2, n+2):
         sheet.cell(row=i, column=1).value = str(i - 1)
-        #TESTING print(sheet.cell(row=i, column=1).value)
-        # figure out how to set text as Bold and Centered
+        # formatting
+        current_cell = sheet.cell(row=i, column=1)
+        current_cell.alignment = Alignment(horizontal='center')
+        current_cell.font = bold_font
 
     # write cells
-    # for each row (i) (starting with B):
-    #   loop from (i)(2) to (i)(end of row)
-    # sheet.max_column
-    for i in range(2, (sheet.max_column + 1):
-        for j in range(2, ):
+    for i in range(2, (sheet.max_row + 1)):
+        for j in range(2, sheet.max_column + 1):
+            formula = "=" + get_column_letter(j) + "1*A" + str(i)
+            sheet.cell(row=i, column=j).value = formula
+            # formatting
+            current_cell = sheet.cell(row=i, column=j)
+            current_cell.alignment = Alignment(horizontal='center')
+
+    print('Multiplication table is complete!')
+    wb.save('multiplication_table_results.xlsx')
+    print('Saved/overwritten to multiplication_table_results.xlsx')
+
+except ValueError:
+        print('Invalid argument: Please enter an int value\n')
+        exit()
+except IndexError:
+        print('Invalid argument: You did not enter an argument\n')
+        exit()
