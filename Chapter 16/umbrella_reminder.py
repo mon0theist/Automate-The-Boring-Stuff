34471f23c9b93c0991759c292201f121c6cc225b
mon0theist <bburns91@gmail.com>
2020-04-01 06:32:16 -0500

    unable to complete umbrella_reminder.py, as the TwilioRestClient method taught in the chapter is now deprecated and no longer works. Hopefully this has been updated in ATBS 2nd Edition.

---
+++ Chapter 16/umbrella_reminder.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+#
+# ATBS - Chapter 16 - Umbrella Reminder
+#
+# Chapter 11 showed you how to use the requests module to scrape data from
+# http://weather.gov/. Write a program that runs just before you wake up in the
+# morning and checks whether it’s raining that day. If so, have the program text
+# you a reminder to pack an umbrella before leaving the house.
+#
+# This project cannot be completed, as the way Twilio works has changed, and
+# the TwilioRestClient method described in the chapter is now deprecated and
+# no longer works. I imagine this is probably updated in ATBS 2nd Edition.
