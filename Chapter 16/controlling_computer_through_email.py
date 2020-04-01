677a7b6b2c2ac5c8aea1319c4f871a1d13ac36bb
mon0theist <bburns91@gmail.com>
2020-04-01 16:11:32 -0500

    modules don't work as taught in the book and are not usable, so problem cannot be completed. I'm also not seeing the completed example program in the downloadable materials.

---
+++ Chapter 16/controlling_computer_through_email.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python
+#
+# ATBS - Chapter 16 - Controlling Your Computer Through Email
+#
+# This program requires imapclient and pyzmail, which no longer work as described
+# and taught in the book, so the modules are not usable. Hopefully this is updated
+# in ATBS 2nd Edition.
