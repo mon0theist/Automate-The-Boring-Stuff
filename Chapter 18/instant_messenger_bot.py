80b2fbdd87220f378af0b9d914ad4ace750fe8df
mon0theist <bburns91@gmail.com>
2020-04-27 18:27:51 -0500

    Started the remaining two practice projects for Chapter 18, they both look like they're going to take a long time...

---
+++ Chapter 18/instant_messenger_bot.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python
+#
+# ATBS - Chapter 18 - Instant Messenger Bot
+#
+# Google Talk, Skype, Yahoo Messenger, AIM, and other instant messaging
+# applications often use proprietary protocols that make it difficult for others
+# to write Python modules that can interact with these programs. But even these
+# proprietary protocols can’t stop you from writing a GUI automation tool.
+#
+# The Google Talk application has a search bar that lets you enter a username on
+# your friend list and open a messaging window when you press ENTER. The keyboard
+# focus automatically moves to the new window. Other instant messenger applications
+# have similar ways to open new message windows. Write a program that will
+# automatically send out a notification message to a select group of people on
+# your friend list. Your program may have to deal with exceptional cases, such as
+# friends being offline, the chat window appearing at different coordinates on
+# the screen, or confirmation boxes that interrupt your messaging. Your program
+# will have to take screen-shots to guide its GUI interaction and adopt ways of
+# detecting when its virtual keystrokes aren’t being sent.
+#
+# Note
+# You may want to set up some fake test accounts so that you don’t accidentally
+# spam your real friends while writing this program.
+#
+# Google Talk is dead, using Google Hangouts instead
+
+import webbrowser, pyautogui
+
+hangouts = webbrowser.open('https://hangouts.google.com')
